506a01e59e7bb1d571616e61489cf41ac0fb2a70
Andreas Beasley <andreas@sapioweb.com>
2019-12-11 21:10:26 -0800

    initial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from './component/main/main.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MainComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
